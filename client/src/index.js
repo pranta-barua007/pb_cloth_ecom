c1519316b39d34112e84ea94ccfa7ac9a29dc383
pranta-barua007 <prantabarua247@gmail.com>
2020-12-09 12:50:39 +0600

    implementation of pwa, added serviceworker, https, updated manifest

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'; //persist component for managing storage
 import { store, persistor } from './redux/store';
+
+import * as serviceWorker from 'serviceWorker';
 
 import './index.css';
 import App from './App';
@@ -23,5 +24,7 @@ ReactDOM.render(
   ,
   document.getElementById('root')
 );
+
+serviceWorker.register();
 
 

822b610367a081a01e27a957a97ae99f4395281d
pranta-barua007 <prantabarua247@gmail.com>
2020-12-10 23:49:57 +0600

    package update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const compression = require('compression');
+//const compression = require('compression');
 var enforce = require('express-sslify');
 
 
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(compression());
+    //app.use(compression());
     app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')));
 

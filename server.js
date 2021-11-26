42f735222ac465815260d8967f792de6b8b3551d
pranta-barua007 <prantabarua247@gmail.com>
2021-11-26 23:54:24 +0600

    added security with helmet

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
-var enforce = require('express-sslify');
+const enforce = require('express-sslify');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
@@ -11,30 +12,30 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+          ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+          "script-src": ["'self'", "checkout.stripe.com", "apis.google.com"],
+          "img-src": ["'self'", "i.ibb.co", "q.stripe.com"],
+          "frame-src": ["'self'", "pb-cloth-db.firebaseapp.com"],
+          "connect-src": ["'self'", "firestore.googleapis.com", "www.googleapis.com"]
+        },
+      },
+  }));
 app.use(cors());
-
+app.use(morgan('combined'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'public')));
 if(process.env.NODE_ENV === 'production') {
-    app.use(compression());
     app.use(enforce.HTTPS({ trustProtoHeader: true }));
-    app.use(express.static(path.join(__dirname, 'client/build')));
+};
 
-    app.get('/service-worker.js', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
-    });
-    
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
-
-app.listen(port, error => {
-    if(error) throw error;
-    console.log('Server is running on port ' + port);
-});
 
 app.post('/payment', (req, res) => {
     const body = {
@@ -49,4 +50,17 @@ app.post('/payment', (req, res) => {
             res.status(200).send({ success: stripeRes});
         }
     });
+});
+
+// app.get('/service-worker.js', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'public', 'service-worker.js'));
+// });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+app.listen(port, error => {
+    if(error) throw error;
+    console.log('Server is running on port ' + port);
 });
